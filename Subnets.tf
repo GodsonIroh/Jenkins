cbb7ae5d11589cb259445b009821b29f4d446a53
Godson Iroh <144743894+GodsonIroh@users.noreply.github.com>
2024-03-11 23:56:10 +0000

    Updates

--- Subnets.tf
+++ Subnets.tf
@@ -7,9 +7,18 @@ resource "aws_subnet" "Pub-Sub1" {
   }
 }
 
-resource "aws_subnet" "Priv-Sub1" {
+resource "aws_subnet" "Pub-Sub2" {
   vpc_id     = aws_vpc.VPC-Testing.id
   cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Pub-Sub2"
+  }
+}
+
+resource "aws_subnet" "Priv-Sub1" {
+  vpc_id     = aws_vpc.VPC-Testing.id
+  cidr_block = "10.0.3.0/24"
 
   tags = {
     Name = "Priv-Sub1"
