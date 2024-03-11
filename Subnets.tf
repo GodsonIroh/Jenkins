3cb4d0f1f952f3a09a36ce692269bbe5450247b5
Godson Iroh <144743894+GodsonIroh@users.noreply.github.com>
2024-03-11 23:45:57 +0000

    Create Subnets.tf
    subnets

---
+++ Subnets.tf
@@ -0,0 +1,17 @@
+resource "aws_subnet" "Pub-Sub1" {
+  vpc_id     = aws_vpc.VPC-Testing.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Pub-Sub1"
+  }
+}
+
+resource "aws_subnet" "Priv-Sub1" {
+  vpc_id     = aws_vpc.VPC-Testing.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Priv-Sub1"
+  }
+}
